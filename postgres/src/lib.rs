6f82dc20a758ae0608956b755d15acbe5881b1fb
importcjj <importcjj@gmail.com>
2019-10-30 12:22:44 +0800

    Fix typo and Add exectuor field to PostgresConnectionManager

--- postgres/src/lib.rs
+++ postgres/src/lib.rs
@@ -2,7 +2,7 @@ use mobc::futures::{FutureExt, TryFutureExt};
 use mobc::AnyFuture;
 use mobc::ConnectionManager;
 use tokio_executor::DefaultExecutor;
-use tokio_executor::Executor;
+use tokio_executor::Executor as TkExecutor;
 pub use tokio_postgres;
 use tokio_postgres::tls::{MakeTlsConnect, TlsConnect};
 use tokio_postgres::Client;
@@ -10,33 +10,52 @@ use tokio_postgres::Config;
 use tokio_postgres::Error;
 use tokio_postgres::Socket;
 
-pub struct PostgresConnectionManager<Tls> {
+pub struct PostgresConnectionManager<Tls, U>
+where
+    U: TkExecutor + Send + Sync + 'static + Clone,
+{
     config: Config,
     tls: Tls,
+    executor: U,
 }
 
-impl<Tls> PostgresConnectionManager<Tls> {
+impl<Tls> PostgresConnectionManager<Tls, DefaultExecutor> {
     pub fn new(config: Config, tls: Tls) -> Self {
         PostgresConnectionManager {
-            config: config,
-            tls: tls,
+            config,
+            tls,
+            executor: DefaultExecutor::current(),
         }
     }
 }
 
-impl<Tls> ConnectionManager for PostgresConnectionManager<Tls>
+impl<Tls, U> PostgresConnectionManager<Tls, U>
+where
+    U: TkExecutor + Send + Sync + 'static + Clone,
+{
+    pub fn new_with_executor(config: Config, tls: Tls, executor: U) -> Self {
+        PostgresConnectionManager {
+            config,
+            tls,
+            executor,
+        }
+    }
+}
+
+impl<Tls, U> ConnectionManager for PostgresConnectionManager<Tls, U>
 where
     Tls: MakeTlsConnect<Socket> + Clone + Send + Sync + 'static,
     <Tls as MakeTlsConnect<Socket>>::Stream: Send + Sync,
     <Tls as MakeTlsConnect<Socket>>::TlsConnect: Send,
     <<Tls as MakeTlsConnect<Socket>>::TlsConnect as TlsConnect<Socket>>::Future: Send,
+    U: TkExecutor + Send + Sync + 'static + Clone,
 {
     type Connection = Client;
-    type Executor = DefaultExecutor;
+    type Executor = U;
     type Error = Error;
 
     fn get_executor(&self) -> Self::Executor {
-        DefaultExecutor::current()
+        self.executor.clone()
     }
 
     fn connect(&self) -> AnyFuture<Self::Connection, Self::Error> {
