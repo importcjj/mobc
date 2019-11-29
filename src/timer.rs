17c26c8037dd049682612eaf052f3d024256ba77
importcjj <importcjj@gmail.com>
2019-11-29 15:30:40 +0800

    Fix examples

--- src/timer.rs
+++ src/timer.rs
@@ -1,5 +1,5 @@
-use futures::FutureExt;
-use futures::StreamExt;
+
+
 #[allow(unused_imports)]
 use std::future::Future;
 use std::time::Duration;
@@ -11,6 +11,7 @@ pub struct AsyncInterval(async_std::stream::Interval);
 #[cfg(feature = "async-std-runtime")]
 impl AsyncInterval {
     pub async fn tick(&mut self) -> Instant {
+        use futures::StreamExt;
         self.0.next().await;
         Instant::now()
     }
@@ -30,12 +31,12 @@ pub(crate) fn interval(dur: Duration) -> AsyncInterval {
 #[cfg(feature = "tokio-runtime")]
 #[cfg(not(feature = "async-std-runtime"))]
 pub(crate) fn timeout(dur: Duration) -> impl Future<Output = ()> {
-    let fut = futures::future::pending::<()>();
-    Box::pin(tokio::time::timeout(dur, fut).map(|_| ()))
+    tokio::time::delay_for(dur)
 }
 
 #[cfg(feature = "async-std-runtime")]
 pub(crate) fn timeout(dur: Duration) -> impl Future<Output = ()> {
+    use futures::FutureExt;
     let fut = futures::future::pending::<()>();
     Box::pin(async_std::future::timeout(dur, fut).map(|_| ()))
 }
